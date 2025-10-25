b0a255e2a328db424b0472c5e786a8f39705b124
Anindya Bose <sap.anindya@gmail.com>
2025-10-25 02:29:55 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("Git Tutorial")
+st.subheader("All In One")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.text_input("Username")
+
+with col2:
+    st.text_input("Password", type="password")
+
+with col3:
+    st.button(label="Submit")
+
+    print("Hijibiji")
